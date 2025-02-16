edcb79f0b8dc5da6cc8e97dce69593f16ac0b3ef
hiteshdarji162021 <hiteshdarji162021@gmail.com>
2025-02-16 14:37:50 +0530

    changes

--- cypress/pages/HomePage.js
+++ cypress/pages/HomePage.js
@@ -1,7 +1,7 @@
 export class HomePage{
     weblocators={
         searchInput:"input[name='search']",
-        clickSearch:".btn.btn-default1",
+        clickSearch:".btn.btn-default",
         product:"img[title='MacBook']",
         addToCart:"Add to Cart",
         successMessage:".alert-success"
